4614d10da8d96913ed29f2033326423957b3e16f
Henryk Paluch <hpaluch@seznam.cz>
2013-06-13 15:57:33 +0200

    Simple stimulus files (for 1st measure cycle only!)

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -76,35 +76,35 @@ ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${OBJECTDIR}/main.o: main.asm  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/main.o.d 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/main.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/main.lst\" -e\"${OBJECTDIR}/main.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/main.o\" \"main.asm\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/main.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG  -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/main.lst\" -e\"${OBJECTDIR}/main.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/main.o\" \"main.asm\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/main.o"
 	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
 ${OBJECTDIR}/intr.o: intr.asm  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/intr.o.d 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/intr.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/intr.lst\" -e\"${OBJECTDIR}/intr.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/intr.o\" \"intr.asm\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/intr.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG  -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/intr.lst\" -e\"${OBJECTDIR}/intr.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/intr.o\" \"intr.asm\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/intr.o"
 	@${FIXDEPS} "${OBJECTDIR}/intr.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
 ${OBJECTDIR}/globals.o: globals.asm  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/globals.o.d 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/globals.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/globals.lst\" -e\"${OBJECTDIR}/globals.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/globals.o\" \"globals.asm\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/globals.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG  -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/globals.lst\" -e\"${OBJECTDIR}/globals.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/globals.o\" \"globals.asm\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/globals.o"
 	@${FIXDEPS} "${OBJECTDIR}/globals.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
 ${OBJECTDIR}/display.o: display.asm  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/display.o.d 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/display.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/display.lst\" -e\"${OBJECTDIR}/display.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/display.o\" \"display.asm\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/display.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG  -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/display.lst\" -e\"${OBJECTDIR}/display.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/display.o\" \"display.asm\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/display.o"
 	@${FIXDEPS} "${OBJECTDIR}/display.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
 ${OBJECTDIR}/ds18b20.o: ds18b20.asm  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/ds18b20.o.d 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/ds18b20.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/ds18b20.lst\" -e\"${OBJECTDIR}/ds18b20.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/ds18b20.o\" \"ds18b20.asm\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/ds18b20.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG  -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/ds18b20.lst\" -e\"${OBJECTDIR}/ds18b20.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/ds18b20.o\" \"ds18b20.asm\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/ds18b20.o"
 	@${FIXDEPS} "${OBJECTDIR}/ds18b20.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
@@ -151,7 +151,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/temp_meter_16f630.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"build/link.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PK3=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/temp_meter_16f630.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"build/link.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/temp_meter_16f630.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/temp_meter_16f630.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
